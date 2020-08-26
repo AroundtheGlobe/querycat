dbca5218c3481d1d475f80d064a9c50b3a78aa52
AroundtheGlobe <development@aroundtheglobe.nl>
2020-08-26 11:45:06 +0200

    install via requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import find_packages, setup
 import os
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='querycat',
       version='1.0',
       author='Erik van Erp',
       author_email='development@aroundtheglobe.nl',
       url='https://github.com/AroundtheGlobe/querycat',
       packages=find_packages(),
-      install_requires=['torch==1.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html',
-                        'transformers',
-                        'scikit-learn',
-                        'spacy>=2.2.0,<2.3.0',
-                        'pyfpgrowth'
-                        ],
+      install_requires=requirements,
       test_suite='tests',
       dependency_links=[
 
