cfa603b65d680c03350cc692e226dfa64e0e7f86
AroundtheGlobe <development@aroundtheglobe.nl>
2020-08-26 12:49:23 +0200

    install via requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+import requirements
+
 from setuptools import find_packages, setup
 import os
+install_requires = []
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+with open('requirements.txt') as fd:
+    for req in requirements.parse(fd):
+        if req.name:
+            name = req.name.replace("-", "_")
+            full_line = name + "".join(["".join(list(spec)) for spec in req.specs])
+            install_requires.append(full_line)
+        else:
+            # a GitHub URL doesn't have a name
+            # I am not sure what to do with this, actually, right now I just ignore it...
+
 
 setup(name='querycat',
       version='1.0',
@@ -10,7 +21,7 @@ setup(name='querycat',
       author_email='development@aroundtheglobe.nl',
       url='https://github.com/AroundtheGlobe/querycat',
       packages=find_packages(),
-      install_requires=requirements,
+      install_requires=install_requires,
       test_suite='tests',
       dependency_links=[
 
