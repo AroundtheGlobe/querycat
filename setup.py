f90d231d78edf81bfe776c285e3b84cd86f9c3f5
AroundtheGlobe <development@aroundtheglobe.nl>
2020-08-26 13:04:30 +0200

    install via requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
-import requirements
-
 from setuptools import find_packages, setup
 import os
-install_requires = []
-
-with open('requirements.txt') as fd:
-    for req in requirements.parse(fd):
-        if req.name:
-            name = req.name.replace("-", "_")
-            full_line = name + "".join(["".join(list(spec)) for spec in req.specs])
-            install_requires.append(full_line)
-        else:
-            # a GitHub URL doesn't have a name
-            # I am not sure what to do with this, actually, right now I just ignore it...
 
 
 setup(name='querycat',
@@ -21,8 +8,7 @@ setup(name='querycat',
       author_email='development@aroundtheglobe.nl',
       url='https://github.com/AroundtheGlobe/querycat',
       packages=find_packages(),
-      install_requires=install_requires,
-      test_suite='tests',
+      install_requires=['transformers','scikit-learn','umap-learn','spacy>=2.2.0,<2.3.0','pyfpgrowth'],
       dependency_links=[
 
       ]
