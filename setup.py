92203b8122250ec9aea907cd2890c9b5c287758e
AroundtheGlobe <development@aroundtheglobe.nl>
2020-08-26 11:28:48 +0200

    add setup.py so it can be installed via requirements.txt within other projects

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+import os
+
+setup(name='querycat',
+      version='1.0',
+      author='Erik van Erp',
+      author_email='development@aroundtheglobe.nl',
+      url='https://github.com/AroundtheGlobe/querycat',
+      packages=find_packages(),
+      install_requires=['torch==1.4.0+cpu',
+                        'transformers',
+                        'scikit-learn',
+                        'spacy>=2.2.0,<2.3.0',
+                        'pyfpgrowth'
+                        ],
+      test_suite='tests',
+      dependency_links=[
+
+      ]
+      )
